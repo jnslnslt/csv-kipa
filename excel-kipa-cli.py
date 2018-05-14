2d1031e9df7985780d6395470cfd1c60ed3ca739
jsnlnslt <jani.sulunsilta@student.tut.fi>
2018-05-14 19:46:26 +0300

    version to read several columns

--- excel-kipa-cli.py
+++ excel-kipa-cli.py
@@ -1,6 +1,5 @@
 import argparse
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
 from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 
@@ -17,22 +16,25 @@ driver.get(args.url) # Open given page
 # read excel file to dataframe
 df = pd.read_excel(args.file, sheet_name = args.sheet)
 
+labels = df.columns.tolist()
 # loop over dataframe rows and fill values to text boxes
 for index, row in df.iterrows():
-    id = "id_" + str(row[0]) + str(row[2])
-    try:
-        element = driver.find_element_by_id(id)
-        element.clear()
-        element.send_keys(row[1])
-    except NoSuchElementException:
-        print("WARN: element with id: " + id + " not found")
+    # loop over dataframe value columns (2nd->)
+    for i in range(1,len(labels)):
+        # parse id from team and label numbers
+        id = "id_" + str(row[0]) + "_" + str(labels[i]) + "-arvo"
+        try:
+            element = driver.find_element_by_id(id)
+            element.clear()
+            element.send_keys(row[i])
+        except NoSuchElementException:
+            print("WARN: element with id: " + id + " not found")
 
 # find save button and click it
 try:
     save = driver.find_element_by_id("Tallenna") # true id?
     element.click()
+    driver.quit() #close browser window
 except:
     print("WARN: save button not found")
-
-# close opened Chrome window
-driver.quit()
+    # if saving fails, browser window is not closed
